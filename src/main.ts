3c8f8d449327c292db0a617f4ddebe3359a1608c
pooriya <pooriya.aba@pm.me>
2021-01-04 13:38:38 +0330

    chore: add swagger and its cli plugin

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       forbidNonWhitelisted: true,
     }),
   );
+
+  // swagger
+  const options = new DocumentBuilder()
+    .setTitle('Workout tracker API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
