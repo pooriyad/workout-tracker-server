73e553682db1483d533312efa2c0a2877e1f6791
pooriya <pooriya.aba@pm.me>
2021-01-14 14:17:21 +0330

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import { AppModule } from './app.module';
 import * as CookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  app.setGlobalPrefix('api');
 
   app.use(CookieParser());
-
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
